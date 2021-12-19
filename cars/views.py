605c47118b5650655bf36af01d806581859679ab
SaadEssam <SaadEssam059@gmail.com>
2021-12-19 20:45:57 +0200

    Search page

--- cars/views.py
+++ cars/views.py
@@ -21,3 +21,13 @@ def car_detail(request, id):
     'single_car' : single_car,
   }
   return render(request, 'cars/car_detail.html', data)
+
+
+
+def search(request):
+  cars = Car.objects.order_by('-created_date')
+  
+  data = {
+    'cars': cars,
+  }
+  return render(request, 'cars/search.html', data)
