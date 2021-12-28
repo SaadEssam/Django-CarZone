60b82c6688e18117f30e017218e3a7d17927b315
SaadEssam <SaadEssam059@gmail.com>
2021-12-28 19:50:00 +0200

    login, register & dashboard Templates

--- carzone/urls.py
+++ carzone/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
     path('cars/', include('cars.urls')),
+    path('accounts/', include('accounts.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
