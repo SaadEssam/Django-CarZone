d4900b914dfb8594156dafb1f12bc38fb02ead91
SaadEssam <SaadEssam059@gmail.com>
2022-01-03 20:26:41 +0200

    Dashboard Functionality

--- carzone/settings.py
+++ carzone/settings.py
@@ -156,3 +156,11 @@ MESSAGE_TAGS = {
 }
 
 SITE_ID = 1
+
+
+# #Email Sending
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = ''
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_USE_TLS = True
--- contacts/views.py
+++ contacts/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Contact
+# from django.core.mail import send_mail
+# from django.contrib.auth.models import User
+
 
 # Create your views here.
+
 def inquiry(request):
   if request.method == 'POST':
     car_id = request.POST['car_id']
@@ -16,9 +20,28 @@ def inquiry(request):
     phone = request.POST['phone']
     message = request.POST['message']
     
+    if request.user.is_authenticated:
+      user_id = request.user.id
+      has_contacted = Contact.objects.all().filter(car_id=car_id, user_id=user_id)
+      if has_contacted:
+        messages.error(request, 'You have already made an inquiry about this car, please wait until we get back to you.')
+        return redirect('/cars/'+car_id)
+  
+
+    
     contact = Contact(car_id=car_id, car_title=car_title, user_id=user_id, first_name=first_name, last_name=last_name, 
                       customer_need=customer_need, city=city, email=email, phone=phone, message=message)
     
+    # admin_info = User.objects.get(is_superuser=True)
+    # admin_email = admin.info.email
+    # send_mail(
+    #   'New Car Inquiry',
+    #   'You have a new inquiry for the car' + car_title + '. Please login to your admin panel for more info.',
+    #   'from@example.com',
+    #   [admin.email],
+    #   fail_silently=False
+    # )
+      
     contact.save()
     messages.success(request, 'Your request has been submitted. We will get back to you soon :)')
     return redirect('/cars/'+car_id)
