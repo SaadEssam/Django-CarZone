548733a3eb381a30e9f9ed97dd22adfaa22441e8
SaadEssam <SaadEssam059@gmail.com>
2022-01-04 19:47:34 +0200

    Contact Page

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Team
 from cars.models import Car
+from django.contrib import messages
+from django.core.mail import send_mail
+from django.contrib.auth.models import User
 # Create your views here.
 
 def home(request):
@@ -41,4 +44,27 @@ def services(request):
 
 
 def contact(request):
+  if request.method == 'POST':
+    name = request.POST['name']
+    email = request.POST['email']
+    subject = request.POST['subject']
+    phone = request.POST['phone']
+    message = request.POST['message']
+    
+    email_subject = 'You have a new message from CarZone ' + subject
+    message_body = 'Name: ' + name + 'Email: ' + email + 'Phone: ' + phone + 'Message: ' + message
+    
+    admin_info = User.objects.get(is_superuser=True)
+    admin_email = admin_info.email
+    
+    send_mail(
+      email_subject,
+      message_body,
+      'SaadEssam059@gmail.com',
+      [admin_email],
+      fail_silently=False,
+    )
+    
+    messages.success(request, 'Thank You for contacting us. We will get back to you soon.')
+    return redirect('contact')
   return render(request, 'pages/contact.html')
